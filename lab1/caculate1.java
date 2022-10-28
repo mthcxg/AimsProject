8da71de61c7114765d455436253139a4ed151700
MaiThuHien <hien.mt200203@sis.hust.edu.vn>
2022-10-28 15:18:04 +0700

    .

--- lab1/caculate1.java
+++ lab1/caculate1.java
@@ -1,15 +1,16 @@
-
+//Mai Thu Hien - 20200203
+//tinh tong hieu tich thuong cua 2 so do nguoi dung nhap
 import java.util.*;  
 public class caculate1 {
     public static void main(String[] args){
         String strNum1, strNum2;
-        Scanner sc= new Scanner(System.in);
+        Scanner sc= new Scanner(System.in); 
         System.out.print("Enter number 1: ");  
-        strNum1 = sc.nextLine(); 
+        strNum1 = sc.nextLine(); //doc string ng dung nhap = strNum1
         System.out.print("Enter number 2: ");  
-        strNum2 = sc.nextLine(); 
+        strNum2 = sc.nextLine();  //doc string nguoi dung nhap = strNum2
         
-        double num1 = Double.parseDouble(strNum1);
+        double num1 = Double.parseDouble(strNum1); //chuyen string thanh double
         double num2 = Double.parseDouble(strNum2);
         double sum, minus, div, mul;
         sum = num1 + num2;
